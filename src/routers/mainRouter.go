b790d1462e721898c63a1b717baf5fc0e39125a8
unknown <muizzuddin334@gmail.com>
2024-02-06 17:07:05 +0700

    add endpoint init

--- src/routers/mainRouter.go
+++ src/routers/mainRouter.go
@@ -1,10 +1,18 @@
 package routers
 
-import "github.com/julienschmidt/httprouter"
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
 
 func Router() *httprouter.Router {
 
 	routers := httprouter.New()
+	routers.GET("/api", func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+
+		w.Write([]byte("selamat datang"))
+	})
 	AuthRoute(routers)
 	AnimeRoute(routers)
 
